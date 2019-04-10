68178c5fc12d4f2b74aa9ef07ab65f92b5d1042b
tarcisio <sousa.tarcisio.s@gmail.com>
2019-04-10 10:47:31 -0300

    Desacoplando projeto dev/prod
    close #9

--- pypro/settings.py
+++ pypro/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY') #'1g2a3vmcjq0%+h5+p&&!^vo#d&dtkd=qzh_vu%qd^wh0b@djn+'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv()) #['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
